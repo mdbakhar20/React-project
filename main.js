4f1f0dee242407c9153d7600e8a44da8dc4f60bf
Mohammed Bakhar <mrbakhar8686@gmail.com>
2026-07-06 17:15:14 +0530

    header designed

--- main.js
+++ main.js
@@ -4,21 +4,36 @@ import ReactDOM from "react-dom/client";
 function Header() {
     return(
         <div className="header">
-            <div>
-             
-                <img  src="https://ik.imagekit.io/moz3rntpv/OIP.webp"/>
-                <h1>hello</h1>
+            <div className="image-logo">
+                <img  src="https://ik.imagekit.io/moz3rntpv/pngtree-food-logo-png-image_8239850.png"/>
+            </div>
+            <div className="search-bar">
+                <input placeholder="search for  resturant or food"/>
+            </div>    
+            <div  className="nav-items">    
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>contact us</li>
+                        <li>cart</li>
+                    </ul>
+                </nav>
             </div>
 
         </div>
     )
 }
 
+function main() {
+    
+}
+
 function App() {
     return( 
         <div>  
            < Header/>
-            
+           <main/>
         </div> 
 )}
 
